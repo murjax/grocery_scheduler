385c7fefe22609c6e35f8090ece3e80f47c66efb
Ryan Murphy <murjax@gmail.com>
2024-07-20 13:09:15 -0400

    Add week navigator

--- lib/grocery_scheduler_web/live/week_view_live/index.ex
+++ lib/grocery_scheduler_web/live/week_view_live/index.ex
@@ -6,7 +6,53 @@ defmodule GrocerySchedulerWeb.WeekViewLive.Index do
   @impl true
   def mount(_params, session, socket) do
     user = GroceryScheduler.Accounts.get_user_by_session_token(session["user_token"])
-    socket = assign(socket, :user_id, user.id)
-    {:ok, stream(socket, :items, Items.list_items())}
+    week_info = Date.utc_today() |> week_info_for_date()
+    socket = assign(
+      socket,
+      user_id: user.id,
+      week_start: week_info.week_start,
+      week_end: week_info.week_end,
+      formatted_week_start: week_info.formatted_week_start,
+      formatted_week_end: week_info.formatted_week_end
+    )
+    {:ok, stream(socket, :items, Items.items_for_week(week_info.week_start))}
+  end
+
+  def handle_event("last_week", _, socket) do
+    week_info = socket.assigns.week_start |> Date.add(-7) |> week_info_for_date()
+    socket = assign(
+      socket,
+      week_start: week_info.week_start,
+      week_end: week_info.week_end,
+      formatted_week_start: week_info.formatted_week_start,
+      formatted_week_end: week_info.formatted_week_end
+    )
+    {:noreply, stream(socket, :items, Items.items_for_week(week_info.week_start), reset: true)}
+  end
+
+  def handle_event("next_week", _, socket) do
+    week_info = socket.assigns.week_start |> Date.add(7) |> week_info_for_date()
+    socket = assign(
+      socket,
+      week_start: week_info.week_start,
+      week_end: week_info.week_end,
+      formatted_week_start: week_info.formatted_week_start,
+      formatted_week_end: week_info.formatted_week_end
+    )
+    {:noreply, stream(socket, :items, Items.items_for_week(week_info.week_start), reset: true)}
+  end
+
+  defp week_info_for_date(date) do
+    week_start = Date.beginning_of_week(date)
+    week_end = Date.end_of_week(date)
+    formatted_week_start = Calendar.strftime(week_start, "%m/%d/%Y")
+    formatted_week_end = Calendar.strftime(week_end, "%m/%d/%Y")
+
+    %{
+      week_start: week_start,
+      week_end: week_end,
+      formatted_week_start: formatted_week_start,
+      formatted_week_end: formatted_week_end
+    }
   end
 end
